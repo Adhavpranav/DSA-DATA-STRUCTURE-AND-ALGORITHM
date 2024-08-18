656a54c66c8a8074f8e6a722edd02e5497162749
Pranav Adhav <142861984+Adhavpranav@users.noreply.github.com>
2024-08-18 12:05:18 +0530

    Update Linear_search.c

--- Searching programs/Linear_search.c
+++ Searching programs/Linear_search.c
@@ -1,34 +1,48 @@
 #include<stdio.h>
-
-int linear_Search(int array[],int limit,int *key)
-{   
-    int i;
-    for(i=0;i<limit;i++)
+int linear_search_count(int array[],int limit,int key)
+{
+    int count=0;
+    for(int i=0;i<limit;i++)
     {
-        if(array[i]==(*key))
-        return ++i;
+        if(array[i]==key)
+        count++;
     }
-        return -1;
+    return count;
 }
 
-void accept(int array[],int limit,int *key)
+void accept(int array[],int limit)
 {
-    printf("\nENTER ELEMENTS:");
+    printf("\nENTER ELEMENTS OF AN ARRAY:");
     for(int i=0;i<limit;i++)
     scanf("%d",&array[i]);
-    printf("\nENTER NUMBER TO FIND:");
-    scanf("%d",key);
+}
+
+void linear_search_Positions(int array[],int limit,int key)
+{
+    for(int i=0;i<limit;i++)
+    {
+    if(array[i]==key)
+    printf("\t%d",i+1);
+    }
 }
 
 int main()
 {
-    int array[100],limit,key;
-    printf("\nENTER LIMIT:");
+    int limit,array[100],key;
+    printf("\nENTER LIMIT OF ARRAY:");
     scanf("%d",&limit);
-    accept(array,limit,&key);
-    int result=linear_Search(array,limit,&key);
-    if(result==-1)
-    printf("\n%d IS NOT FOUND IN ARRAY.",key);
+    accept(array,limit);
+    printf("\nENTER NUMBER FOR SEARCH:");
+    scanf("%d",&key);
+    int count=linear_search_count(array,limit,key);
+    if(count!=0)
+    {
+        printf("\nNUMBER IS FOUND.");
+        printf("\nNUMBER OF OCCURENCES: %d",count);
+        printf("\nPOSITIONS OF NUMBER WHERE IT IS FOUND:");
+        linear_search_Positions(array,limit,key);   
+    }
     else
-    printf("\n%d IS FOUND AT %d POSITION IN ARRAY.",key,result);
+    printf("\nNUMBER IS NOT FOUND.");
+    return 0;
 }
