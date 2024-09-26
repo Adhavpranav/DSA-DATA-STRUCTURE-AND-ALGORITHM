254f63a2b0ee480e571f50cb9768d6e743e61638
Pranav Adhav <142861984+Adhavpranav@users.noreply.github.com>
2024-09-26 06:24:05 +0530

    Update CountingSort.c

--- Sorting programs/CountingSort.c
+++ Sorting programs/CountingSort.c
@@ -1,56 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-void countsortarray(int *array, int limit, int k)
+#include<stdio.h>
+
+void display(int array[],int limit)
 {
-    int *b,*count,i;
-    b=(int*)malloc(limit*sizeof(int));
-    count=(int*)malloc((k+1)*sizeof(int));
-    for (i = 0; i <= k; i++)
-    {
-        count[i] = 0;
-    }
-    for (i = 0; i < limit; i++)
+    int i;
+    printf("\nSORTED ARRAY:");
+    for(i=0;i<limit;i++)
+    printf("\t%d",array[i]);
+}
+
+void accept(int array[],int limit)
+{
+
+    int i;
+    printf("\nENTER ELEMENTS OF AN ARRAY:");
+    for(i=0;i<limit;i++)
+    scanf("%d",&array[i]);
+}
+
+void countsort(int array[],int limit,int max)
+{
+    int count[100]={0},b[100],i;
+    for(i=0;i<limit;i++)
     {
         ++count[array[i]];
     }
-    for (i = 1; i <=k;i++)
+    for(i=1;i<=max;i++)
     {
         count[i]=count[i]+count[i-1];
     }
-    for (i=limit-1;i>=0;i--)
+    for(i=limit-1;i>=0;i--)
     {
         b[--count[array[i]]]=array[i];
     }
-    for (i = 0; i < limit; i++)
-    {
-        array[i] = b[i];
-    }
-    free(b);
-    free(count);
+    for(i=0;i<limit;i++)
+    array[i]=b[i];
 }
 
 int main()
 {
-    int *array, limit, i, k;
-    printf("ENTER LIMIT:");
-    scanf(" %d", &limit);
-    array = (int *)malloc(limit * sizeof(int));
-    printf("ENTER ARRAY ELEMENTS:");
-    for (i = 0; i < limit; i++)
-    {
-        scanf("%d", &array[i]);
-    }
-    k = array[0];
-    for (i = 1; i < limit; i++)
-    {
-        if (array[i] > k)
-            k = array[i];
-    }
-    countsortarray(array, limit, k);
-    printf("\nSORTED ARRAY:");
-    for (i = 0; i < limit; i++)
-        printf("%d\t", array[i]);
-    printf("\n");
-    free(array);
-    return 0;
+    int array[100],limit,max,i;
+    printf("\nENTER THE LIMIT: ");
+    scanf("%d",&limit);
+    accept(array,limit);
+    max=array[0];
+    for(i=1;i<limit;i++)
+    if(array[i]>max)
+    max=array[i];
+    countsort(array,limit,max);
+    display(array,limit);
 }
