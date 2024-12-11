be9d07c35b7fdac8fdc37f38ab2f0128cf01a3ea
Pranav Adhav <142861984+Adhavpranav@users.noreply.github.com>
2024-12-11 07:43:38 +0530

    Update Polynomial_Addition.c

--- Linklist programs/Polynomial_Addition.c
+++ Linklist programs/Polynomial_Addition.c
@@ -1,112 +1,116 @@
-
 #include<stdio.h>
 #include<stdlib.h>
 struct node
 {
-	int coef,pow;
-	struct node *next;
+    int coef,pow;
+    struct node* next;
 };
-struct node *create(struct node *head)
+
+struct node*create(struct node*head)
 {
-	int n,i;
-	struct node *newnode,*temp;
-	printf("Enter how many terms:");
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	{
-		newnode=(struct node *)malloc(sizeof(struct node));
-		printf("Enter coef and power:");
-		scanf("%d%d",&newnode->coef,&newnode->pow);
-		newnode->next=NULL;
-		if(head==NULL)
-		{
-			head=temp=newnode;
-		}
-		else
-		{
-			temp->next=newnode;
-			temp=newnode;
-		}
-	}
-	return head;
+    int limit,i;
+    struct node* newnode,*temp;
+    printf("\nENTER HOW MANY TERMS:");
+    scanf("%d",&limit);
+    for(i=0;i<limit;i++)
+    {
+        newnode=(struct node*)malloc(sizeof(struct node));
+        printf("ENTER COEF AND POWER:");
+        scanf("%d%d",&newnode->coef,&newnode->pow);
+        newnode->next=NULL;
+        if(head==NULL)
+        {
+            head=temp=newnode;
+        }
+        else
+        {
+            temp->next=newnode;
+            temp=newnode;
+        }
+    }
+    return head; 
 }
-void disp(struct node *head)
+
+void display(struct node* head)
 {
-	struct node *temp;
-	for(temp=head;temp!=NULL;temp=temp->next)
-	{
-		printf("%dx^%d\t",temp->coef,temp->pow);
-		if(temp->next!=NULL)
-		  printf("+");
-	}
+    struct node* temp;
+    for(temp=head;temp!=NULL;temp=temp->next)
+    {
+        printf("%dx^%d",temp->coef,temp->pow);
+        if(temp->next!=NULL)
+        printf("+");
+    }
 }
-void add(struct node *head1,struct node *head2,struct node *head3)
+
+struct node*add(struct node*head1,struct node*head2)
 {
-	struct node *temp1=head1,*temp2=head2,*temp3,*newnode;
-	while(temp1!=NULL&&temp2!=NULL)
-	{
-		newnode=(struct node *)malloc(sizeof(struct node));
-		newnode->next=NULL;
-		if(temp1->pow>temp2->pow)
-		{
-			newnode->coef=temp1->coef;
-			newnode->pow=temp1->pow;
-			temp1=temp1->next;
-		}
-		else if(temp1->pow<temp2->pow)
-		{
-			newnode->coef=temp2->coef;
-			newnode->pow=temp2->pow;
-			temp2=temp2->next;
-		}
-		else
-		{
-			newnode->coef=temp1->coef+temp2->coef;
-			newnode->pow=temp2->pow;//or newnode->pow=temp1->pow
-			temp1=temp1->next;
-			temp2=temp2->next;
-		}
-		if(head3==NULL)
-		{
-			head3=temp3=newnode;
-		}
-		temp3->next=newnode;
-		temp3=newnode;
-	}
-	while(temp1!=NULL)
-	{
-		newnode=(struct node *)malloc(sizeof(struct node));
-		newnode->next=NULL;
-		newnode->coef=temp1->coef;
-		newnode->pow=temp1->pow;
-		temp1=temp1->next;
-		temp3->next=newnode;
-		temp3=newnode;
-	}
-	while(temp2!=NULL)
-	{
-		newnode=(struct node *)malloc(sizeof(struct node));
-		newnode->next=NULL;
-		newnode->coef=temp2->coef;
-		newnode->pow=temp2->pow;
-		temp2=temp2->next;
-		temp3->next=newnode;
-		temp3=newnode;
-	}
-	disp(head3);
+    struct node*head=NULL;
+    struct node*temp1=head1,*temp2=head2,*newnode,*temp=NULL;
+    while(temp1!=NULL && temp2!=NULL)
+    {
+        newnode=(struct node*)malloc(sizeof(struct node));
+        newnode->next=NULL;
+        if(head==NULL)
+        {
+           head=temp=newnode; 
+        }
+        else
+        {
+        temp->next=newnode;
+        temp=newnode;
+        }
+        if(temp1->pow>temp2->pow)
+        {
+            newnode->coef=temp1->coef;
+            newnode->pow=temp1->pow;
+            temp1=temp1->next;
+        }
+        else if(temp2->pow>temp1->pow)
+        {
+            newnode->coef=temp2->coef;
+            newnode->pow=temp2->pow;
+            temp2=temp2->next;
+        }
+        else
+        {
+            newnode->coef=temp1->coef+temp2->coef;
+            newnode->pow=temp1->pow;
+            temp1=temp1->next;
+            temp2=temp2->next;
+        }
+    }
+    while(temp1!=NULL)
+    {
+        newnode=(struct node*)malloc(sizeof(struct node));
+        newnode->next=NULL;
+        newnode->coef=temp1->coef;
+        newnode->pow=temp1->pow;
+        temp->next=newnode;
+        temp=newnode;
+        temp1=temp1->next;
+    }
+     while(temp2!=NULL)
+    {
+        newnode=(struct node*)malloc(sizeof(struct node));
+        newnode->next=NULL;
+        newnode->coef=temp2->coef;
+        newnode->pow=temp2->pow;
+        temp->next=newnode;
+        temp2=temp2->next;
+        temp=newnode;
+    }
+    return head;
 }
+
 int main()
 {
-	struct node *head1=NULL,*head2=NULL,*head3=NULL;
-	printf("Polynomial 1:");
-	head1=create(head1);
-	printf("Polynomial 2:");
-	head2=create(head2);
-	printf("\nPolynimial 1:");
-	disp(head1);
-	printf("\nPolynimial 2:");
-	disp(head2);
-	printf("\nAddition of Polynomial:");
-    
-	add(head1,head2,head3);
+    struct node*head1=NULL,*head2=NULL,*head=NULL;
+    printf("\nENTER POLYNOMIAL FIRST:");
+    head1=create(head1);
+    printf("\nENTER POLYNOMIAL SECOND:");
+    head2=create(head2);
+    printf("\nADDITION OF POLYNOMIAL IS:");
+    head=add(head1,head2);
+    display(head);
+    return 0;
 }
